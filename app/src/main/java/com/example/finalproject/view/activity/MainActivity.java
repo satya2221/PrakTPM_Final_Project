18ebbdc0bd0ab013fcec1c1fc4859252898f6fd5
satya2221 <satyaghifari12@gmail.com>
2021-06-19 20:03:20 +0700

    Upload

---
+++ app/src/main/java/com/example/finalproject/view/activity/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.finalproject.view.activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.finalproject.R;
+import com.example.finalproject.view.fragment.AddQuoteFragment;
+import com.example.finalproject.view.fragment.FavQsFragment;
+import com.example.finalproject.view.fragment.QuoteFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    private BottomNavigationView bottomNavigationView;
+//    private Fragment selectedFragment = new FavQsFragment();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new FavQsFragment());
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_favqs:
+                //selectedFragment = new FavQsFragment();
+                loadFragment(new FavQsFragment());
+                break;
+            case R.id.menu_quote:
+                //selectedFragment = new QuoteFragment();
+                loadFragment(new QuoteFragment());
+                break;
+            case R.id.menu_add:
+                //selectedFragment = new AddQuoteFragment();
+                loadFragment(new AddQuoteFragment());
+                break;
+        }
+        return true;
+    }
+
+    private boolean loadFragment(Fragment selectedFragment) {
+        if(selectedFragment != null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_main,selectedFragment)
+                    .commit();
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
